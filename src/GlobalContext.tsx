04cb3062842dca21e1efd513e6d3798fcc58aacb
Olivier <olivierchemla@gmail.com>
2020-12-02 16:49:13 +0100

    Remove placeholder context

--- src/GlobalContext.tsx
+++ src/GlobalContext.tsx
@@ -8,45 +8,7 @@ export const GlobalContext = createContext<any | undefined>(undefined);
 
 export const GlobalProvider = (props: Props) => {
   // Users State
-  const [users, setUsers] = useState([
-    {
-      username: "Peter",
-      color: "#364F6B",
-      items: [
-        { name: "Bananas", quantity: 10, price: 1 },
-        { name: "Strawberry", quantity: 20, price: 1.22 },
-        { name: "Milk", quantity: 6, price: 1.19 },
-      ],
-    },
-    {
-      username: "William",
-      color: "#3FC1C9",
-
-      items: [
-        { name: "Brocolis 🥦", quantity: 10, price: 1.39 },
-        { name: "Carottes 🥕", quantity: 20, price: 1.1 },
-        { name: "Honey 🍯", quantity: 2, price: 3.19 },
-      ],
-    },
-    {
-      username: "Lorenzo",
-      color: "#FC5185",
-      items: [
-        { name: "Pizza 🍕", quantity: 3, price: 2.39 },
-        { name: "Comcumber 🥒", quantity: 3, price: 1.1 },
-        { name: "Baguettes 🥖", quantity: 2, price: 0.99 },
-      ],
-    },
-    {
-      username: "Daniela",
-      color: "#FBC029",
-      items: [
-        { name: "Pizza 🍕", quantity: 3, price: 2.39 },
-        { name: "Comcumber 🥒", quantity: 3, price: 1.1 },
-        { name: "Baguettes 🥖", quantity: 2, price: 0.99 },
-      ],
-    },
-  ]);
+  const [users, setUsers] = useState([]);
 
   return (
     <GlobalContext.Provider value={[users, setUsers]}>
